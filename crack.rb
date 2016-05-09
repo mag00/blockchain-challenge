1ed4d4ed449de793cbe3325b37fda48cb0f44048
Ryan McGeehan <billygoatmagoo@gmail.com>
2016-05-09 10:24:11 -0700

    script totally didn't even work

--- crack.rb
+++ crack.rb
@@ -10,22 +10,19 @@ argv = ARGF.argv
 leader = argv[0].to_i(16)
 difficulty = argv[1].to_i(16)
 
-
 #Seed with random data
 winner = rand()
 
 #SHA3 hash (it's actually keccak) and change to integer
 challenge = Digest::SHA3.hexdigest(winner.to_s,256).to_i(16)
-
 #Search for a collision that is less than the target difficulty to win
 while ((challenge ^ leader) > difficulty) do
   winner = rand()
-  challenge = Digest::SHA3.digest(winner.to_s,256).to_i(16)
+  challenge = Digest::SHA3.hexdigest(winner.to_s,256).to_i(16)
 end
 
-#We escaped the loop, print the string.
+#We escaped the loop, print the winning string.
 puts "Submit this string to the 'Overthrow' function, quick!: #{winner}"
-
 #If this took a long time, be sure to submit it quickly before a competitor changes the target hash on you!
 
 end
